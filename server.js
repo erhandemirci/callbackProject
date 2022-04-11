fef70b0ae20c2ea52cffaea9b95c525de0ac5807
Erhan <demirci.erhan@windowslive.com>
2022-04-11 12:15:15 +0300

    Update server.js

--- server.js
+++ server.js
@@ -15,22 +15,6 @@ const channelId = "UC1zAttFQKikWoKH3Vb39ETA";
     var router = express.Router();
 
 
-router.get('/callback', async function(req, res, next) {
-      try {
-        const searchQuery = req.query.search_query;
-        const url = `${apiUrl}/search?channelId=${channelId}&key=${apiKey}`;
-    console.log(url);
-          
-       
-        const response = await axios.post(url);
-        //const titles = response.data.items.map((item) => item.snippet.title);
-    
-        //res.send(titles);
-    res.send(response.data);
-      } catch (err) {
-        next(err);
-      }
-    });
 
     router.get('/channel', async function(req, res, next) {
       try {
